47581eba086923a20ba0ec9a5aff9336c2a6c933
vilio316 <atembarry12@gmail.com>
2023-02-07 11:22:25 +0100

    Here We Go

--- todoscript.js
+++ todoscript.js
@@ -9,6 +9,61 @@ for (let i = 0; i < badges.length; i++) {
 badgeUpdate();
 //
 
+// Movement, Delete Logic
+let tasks_undone= document.querySelector("#todo");
+let tasks_done= document.querySelector("#done");
+//
+
+//Tracking The Buttons
+let done_button = document.querySelectorAll(".madone");
+let taskChildren;
+let tasks_classless = document.querySelectorAll(".task");
+for (let i = 0; i < done_button.length; i++) {
+    done_button[i].textContent = "Mark as Done";
+     // Listeners for Category Switching Here:
+    done_button[i].addEventListener("click", ()=> {
+        if(done_button[i].textContent == "Mark as Done"){
+            done_button[i].textContent = "Mark as Undone";
+            tasks_done.appendChild(tasks_classless[i]);
+        }
+        else{done_button[i].textContent = "Mark as Done";
+        tasks_undone.appendChild(tasks_classless[i]);
+    }
+    badgeUpdate();
+    })
+}
+
+let delete_button = document.querySelectorAll(".delete");
+for(let j=0; j < delete_button.length; j++){
+    delete_button[j].textContent= "Delete";
+    delete_button[j].onclick = ()=> {
+        taskChildren = Array.from(taskContainer.children);
+        let task_pos = taskChildren.indexOf(tasks_classless[j])
+        if(task_pos !== -1){
+       taskContainer.removeChild(tasks_classless[j]);
+        }
+        else{ doneContainer.removeChild(tasks_classless[j]);}
+    badgeUpdate();
+    }
+}
+
+//
+
+
+//Task Headers
+function descUpdate(){
+let taskheaders = document.querySelectorAll(".task-head");
+let taskbodies = document.querySelectorAll(".task-description");
+for (let b = 0 ; b < taskheaders.length; b++) {
+taskheaders[b].addEventListener("click", ()=>{
+    if(taskbodies[b].className.indexOf("show") == -1){taskbodies[b].classList.replace("none", "show")}
+else{taskbodies[b].classList.replace("show", "none")}
+})
+}
+}
+//
+
+
 //To Do and Done Lists
 let taskContainer= document.getElementById("todo");
 let doneContainer = document.getElementById("done");
@@ -43,44 +98,7 @@ slider.addEventListener('click', ()=> {
 )
 //
 
-//Deletion and Movement Logic
-let tasks_undone= document.querySelector("#todo");
-let tasks_done= document.querySelector("#done");
 
-//Tracking The Buttons
-let done_button = document.querySelectorAll(".madone");
-let taskChildren;
-let tasks_classless = document.querySelectorAll(".task");
-for (let i = 0; i < done_button.length; i++) {
-    done_button[i].textContent = "Mark as Done";
-    
-    // Listeners for Category Switching Here:
-    done_button[i].addEventListener("click", ()=> {
-        if(done_button[i].textContent == "Mark as Done"){
-            done_button[i].textContent = "Mark as Undone";
-            tasks_done.appendChild(tasks_classless[i]);
-        }
-        else{done_button[i].textContent = "Mark as Done";
-        tasks_undone.appendChild(tasks_classless[i]);
-    }
-    badgeUpdate();
-    })
-}
-
-let delete_button = document.querySelectorAll(".delete");
-for(let j=0; j < delete_button.length; j++){
-    delete_button[j].textContent= "Delete";
-    delete_button[j].onclick = ()=> {
-        taskChildren = Array.from(taskContainer.children);
-        let task_pos = taskChildren.indexOf(tasks_classless[j])
-        if(task_pos !== -1){
-       taskContainer.removeChild(tasks_classless[j]);
-        }
-        else{ doneContainer.removeChild(tasks_classless[j]);}
-    badgeUpdate();
-    }
-}
-//
 
 //Task Category Div Code
 let category = document.querySelectorAll(".task-type");
@@ -101,19 +119,7 @@ for (let i = 0; i < categories.length; i++) {
 }
 
 
-//Task Headers
-let taskheaders = document.querySelectorAll(".task-head");
-for (let b = 0 ; b < taskheaders.length; b++) {
-let taskbodies = document.querySelectorAll(".task-description");
-taskheaders[b].addEventListener("click", ()=>{
-    if(taskbodies[b].className.indexOf("show") == -1){
-    taskbodies[b].classList.replace("none", "show")
-}
-else{taskbodies[b].classList.replace("show", "none")}
-}
-)
-}
-//
+
 
 //
 let close_button =document.querySelector("#close_button");
@@ -151,7 +157,10 @@ makeTask.addEventListener("click", ()=> {
 div_flexBetween.appendChild(span_taskHead); div_flexBetween.appendChild(div_2);
 div_1.appendChild(div_flexBetween); div_1.appendChild(div_taskDescription);
 li_task.appendChild(div_1);
-console.log(li_task);
 tasks_undone.appendChild(li_task);
 badgeUpdate();
+descUpdate();
 })
+
+makeTask.addEventListener("click", ()=> {let tasks= document.getElementById("task-create");
+if(tasks.className.includes('show')) {tasks.classList.replace("show","none")}});
